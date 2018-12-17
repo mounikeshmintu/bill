5e980941f113dc89ed73e90de1a368c6951d9717
mounikeshmintu <32958325+mounikeshmintu@users.noreply.github.com>
2018-12-17 15:16:22 +0530

    link added

--- billing/urls.py
+++ billing/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('pro/', include('bill.api.urls',namespace='order'))
+    path('pro/', include('bill.api.urls',namespace='order'))
 ]
